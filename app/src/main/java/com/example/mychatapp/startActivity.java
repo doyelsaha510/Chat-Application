b869d8f8b4b9edfced0f8276ecbf568fdf643a6c
seaofknowledge <40788640+seaofknowledge@users.noreply.github.com>
2019-06-18 12:43:46 +0530

    done

--- app/src/main/java/com/example/mychatapp/startActivity.java
+++ app/src/main/java/com/example/mychatapp/startActivity.java
@@ -1,7 +1,9 @@
 package com.example.mychatapp;
 
 import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -9,29 +11,37 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class startActivity extends AppCompatActivity {
-    Button mRegbtn,mlgnbtn;
+    Button mRegbtn, mlgnbtn;
     RelativeLayout relativeLayout;
     ImageView icon;
-    Animation banimation,ianimation;
+    Animation banimation, ianimation, tanimation;
+    TextView appname;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        relativeLayout=findViewById(R.id.btns);
-        icon=findViewById(R.id.icon);
-        banimation= AnimationUtils.loadAnimation(this,R.anim.frombutton);
-        ianimation= AnimationUtils.loadAnimation(this,R.anim.fromicon);
+        relativeLayout = findViewById(R.id.btns);
+        icon = findViewById(R.id.icon);
+        appname = findViewById(R.id.appname);
+        banimation = AnimationUtils.loadAnimation(this, R.anim.frombutton);
+        ianimation = AnimationUtils.loadAnimation(this, R.anim.fromicon);
+        tanimation = AnimationUtils.loadAnimation(this, R.anim.fromicon);
+
+        appname.setAnimation(tanimation);
         relativeLayout.setAnimation(banimation);
         icon.setAnimation(ianimation);
 
-        mRegbtn=findViewById(R.id.btnnewacc);
-        mlgnbtn=findViewById(R.id.btnlogin);
+
+        mRegbtn = findViewById(R.id.btnnewacc);
+        mlgnbtn = findViewById(R.id.btnlogin);
         mRegbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent reg_intent=new Intent(startActivity.this,RegisterActivity.class);
+                Intent reg_intent = new Intent(startActivity.this, RegisterActivity.class);
                 startActivity(reg_intent);
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
@@ -39,7 +49,7 @@ public class startActivity extends AppCompatActivity {
         mlgnbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent reg_intent=new Intent(startActivity.this,LoginActivity.class);
+                Intent reg_intent = new Intent(startActivity.this, LoginActivity.class);
                 startActivity(reg_intent);
                 overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
             }
--- app/src/main/java/com/example/mychatapp/statusactivity.java
+++ app/src/main/java/com/example/mychatapp/statusactivity.java
@@ -1,69 +1,74 @@
 package com.example.mychatapp;
 
 import android.app.ProgressDialog;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class statusactivity extends AppCompatActivity {
-Button newstatusbtn;
-EditText statustext;
-Toolbar toolbar;
+    MaterialButton newstatusbtn;
+    TextInputEditText statustext;
+    Toolbar toolbar;
     DatabaseReference datareference;
     FirebaseUser currentUser;
-ProgressDialog progressDialog;
+    ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statusactivity);
-        newstatusbtn=findViewById(R.id.changestatus);
-        statustext=findViewById(R.id.statusvalue);
+        newstatusbtn = findViewById(R.id.changestatus);
+        statustext = findViewById(R.id.statusvalue);
 
-        currentUser= FirebaseAuth.getInstance().getCurrentUser();
-        String current_uid=currentUser.getUid();
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String current_uid = currentUser.getUid();
 
-        datareference= FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
+        datareference = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
 
-        toolbar=findViewById(R.id.statuappbar);
+        toolbar = findViewById(R.id.statuappbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Account Status");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        String status_value=getIntent().getStringExtra("status");
+        String status_value = getIntent().getStringExtra("status");
 
         statustext.setText(status_value);
         newstatusbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //progress
-                progressDialog=new ProgressDialog(statusactivity.this);
+                progressDialog = new ProgressDialog(statusactivity.this);
                 progressDialog.setTitle("Saving Changes");
                 progressDialog.setMessage("Pls wait while we save the changes");
                 progressDialog.show();
 
-                String status=statustext.getText().toString();
+                String status = statustext.getText().toString();
 
-                    datareference.child("status").setValue(status).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful())
-                            {
-                                progressDialog.dismiss();
-                            }
+                datareference.child("status").setValue(status).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
                         }
-                    });
-
+                    }
+                });
 
 
             }
